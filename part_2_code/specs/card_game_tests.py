4241cdd80d93b72d25ab97ac9ca82803c506598a
David Valdez Taylor <davidvaldeztaylor@gmail.com>
2022-10-03 20:02:07 +0100

    added tests

--- part_2_code/specs/card_game_tests.py
+++ part_2_code/specs/card_game_tests.py
@@ -2,5 +2,23 @@ import unittest
 from src.card import Card
 from src.card_game import CardGame
 
-class TestCardGame:
-    pass
+class TestCardGame(unittest.TestCase):
+
+    def setUp(self):
+        self.card1 = Card('hearts', 1)
+        self.card2 = Card('clubs', 11)
+        self.card3 = Card('spades',3)
+
+        self.cardlist = (self.card1, self.card2, self.card3)
+
+    def test_check_for_ace(self):
+        self.assertEqual(True, CardGame.check_for_ace(self, self.card1))
+
+    def test_check_for_ace2(self):
+        self.assertEqual(False, CardGame.check_for_ace(self, self.card2))
+
+    def test_highest_card(self):
+        self.assertEqual(self.card2, CardGame.highest_card(self, self.card2, self.card3))
+
+    def test_cards_total(self):
+        self.assertEqual("You have a total of 15", CardGame.cards_total(self, self.cardlist))
--- part_2_code/src/card_game.py
+++ part_2_code/src/card_game.py
@@ -13,17 +13,15 @@ class CardGame:
     else:
       return False
    
-
   def highest_card(self, card1, card2):
     if card1.value > card2.value:
       return card1
     else:
       return card2
 
-
-
   def cards_total(self, cards):
     total = 0
     for card in cards:
       total += card.value
-    return "You have a total of" + total
+    return ("You have a total of " + str(total))
+    
